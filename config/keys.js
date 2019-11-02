c76667d23dfb1d9e19619e06cc7e0a2012a35510
Guido della Rocca <guidellarocca@MBP-di-Guido.station>
2019-11-02 18:15:01 +0100

    first

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '64279357674-ll92n2965milpsndqai0iivfv6r93gd4.apps.googleusercontent.com',
+        clientSecret: 'a9UhLRNBXG-gKMdreCsNRlws'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://guidellarocca:test@cluster0-glky4.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session: {
+        cookieKey: 'thenetninjaisawesomeiguess'
+    }
+};
